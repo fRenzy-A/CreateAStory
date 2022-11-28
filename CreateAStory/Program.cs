2985bc3b66dad5d32da16e292ba040c1aa71e7c3
fRenzy <fRenzyA05@gmail.com>
2022-11-28 14:29:42 -0400

    more stuff added

--- CreateAStory/Program.cs
+++ CreateAStory/Program.cs
@@ -10,18 +10,60 @@ namespace CreateAStory
     {
         static void Main(string[] args)
         {
-
-
-
-
+            InteractiveStory();
         }
 
-        static string[] story = new string[]  { "First.;Second.;1;2;", } ;
-
-
-        static void ChoiceControl()
+        static void InteractiveStory()
         {
+            //string[] story = new string[] { "First;Second.;1;2;" };
+            Console.WriteLine("Press Enter key to go");
 
+            string story =  "First Page;Second Page;Third Page;Fourth Page" ;
+            string choice = "1;2";
+            string[] splitter = story.Split(';');
+            string[] choicesplit = choice.Split(';');
+
+            ConsoleKeyInfo KeyInfo;
+            KeyInfo = Console.ReadKey(true);//KeyInfo.Key
+            int x = 0;
+            bool Game = false;
+            if (KeyInfo.Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                Game = true;
+            }
+            while (Game == true)
+            {
+                Console.WriteLine(splitter[x]);
+                foreach (string p in choicesplit)
+                {
+                    Console.WriteLine($"{p}");
+                }
+                if (KeyInfo.Key == ConsoleKey.Z)
+                {
+                    x++;
+                    //Console.WriteLine(splitter[1]);
+                }
+                if (KeyInfo.Key == ConsoleKey.X)
+                {
+                    x = x + 2;
+                    //Console.WriteLine(splitter[2]);
+                }
+                
+                Console.ReadKey(true);
+            }
+            
+            /*foreach (var s in splitter)
+            {
+                Console.WriteLine($"{s}");
+                foreach (var p in choicesplit)
+                {
+                    Console.WriteLine($"{p}");
+                }
+                
+            }*/
+
+            Console.ReadKey(true);
         }
     }
 }
