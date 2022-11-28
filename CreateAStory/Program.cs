717a20656ba9fdbae405554d801372526c62f2aa
Nathan A <fRenzyA05@gmail.com>
2022-11-27 22:30:27 -0400

    Initial commit

---
+++ CreateAStory/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAStory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+
+        static string[] story = new string[]  { "First.;Second.;1;2;", } ;
+
+
+        static void ChoiceControl()
+        {
+
+        }
+    }
+}
