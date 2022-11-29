caf593247a41e54fe803b1593bb3b36d95316a8f
fRenzy <fRenzyA05@gmail.com>
2022-11-29 15:42:42 -0400

    loop done

--- CreateAStory/Program.cs
+++ CreateAStory/Program.cs
@@ -9,61 +9,46 @@ namespace CreateAStory
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {           
             InteractiveStory();
         }
-
+        static int x = 0;
         static void InteractiveStory()
         {
-            //string[] story = new string[] { "First;Second.;1;2;" };
             Console.WriteLine("Press Enter key to go");
 
-            string story =  "First Page;Second Page;Third Page;Fourth Page" ;
-            string choice = "1;2";
+            string story =  "First Page;Second Page;Third Page;Fourth Page;5;6;7;8;9;10" ;
             string[] splitter = story.Split(';');
-            string[] choicesplit = choice.Split(';');
+
 
             ConsoleKeyInfo KeyInfo;
             KeyInfo = Console.ReadKey(true);//KeyInfo.Key
-            int x = 0;
-            bool Game = false;
+            
+            bool GameStart = false;
             if (KeyInfo.Key == ConsoleKey.Enter)
             {
                 Console.Clear();
-                Game = true;
-            }
-            while (Game == true)
-            {
+                GameStart = true;
                 Console.WriteLine(splitter[x]);
-                foreach (string p in choicesplit)
+                while (GameStart == true)
                 {
-                    Console.WriteLine($"{p}");
+                    ConsoleKeyInfo KeyInfo1;
+                    KeyInfo1 = Console.ReadKey(true);
+
+                    if (KeyInfo1.Key == ConsoleKey.A)
+                    {
+                        x++;
+                    }
+                    else if (KeyInfo1.Key == ConsoleKey.S)
+                    {
+                        x = x + 2;
+                    }
+                    Console.WriteLine(splitter[x]);
                 }
-                if (KeyInfo.Key == ConsoleKey.Z)
-                {
-                    x++;
-                    //Console.WriteLine(splitter[1]);
-                }
-                if (KeyInfo.Key == ConsoleKey.X)
-                {
-                    x = x + 2;
-                    //Console.WriteLine(splitter[2]);
-                }
-                
                 Console.ReadKey(true);
             }
             
-            /*foreach (var s in splitter)
-            {
-                Console.WriteLine($"{s}");
-                foreach (var p in choicesplit)
-                {
-                    Console.WriteLine($"{p}");
-                }
-                
-            }*/
-
-            Console.ReadKey(true);
+           
         }
     }
 }
