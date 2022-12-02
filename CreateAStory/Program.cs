a16658167668d55ef70b39a8d07b4f86e43da433
Nathan A <fRenzyA05@gmail.com>
2022-12-02 07:44:42 -0400

    Inputs half working

--- CreateAStory/Program.cs
+++ CreateAStory/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,28 +14,68 @@ namespace CreateAStory
     {
         static void Main(string[] args)
         {           
-            Story();
-            Console.ReadKey(true);
+            InteractiveStory();
         }
 
         static void InteractiveStory()
         {
-            Story();
+            string[] story = { "FirstPage;FlipToPage2;FlipToPage3;1;2", "SecondPage;FlipToPage4;FlipToPage6;3;4", "ThirdPage;FlipToPage5;FlipToPage6;4;5", "4thPage", "5thPage", "6thPage" };
 
+
+            bool GameStart = false;
+
+            
+            
             ConsoleKeyInfo ReadKey;
             ReadKey = Console.ReadKey(true);
 
-            Console.WriteLine(story[0]);
-       
-            if (ReadKey.Key == ConsoleKey.A)
+            Console.WriteLine("Press Enter to start");
+            if (ReadKey.Key == ConsoleKey.Enter)
+            {        
+                GameStart = true;
+            }
+            int pg = 0;
+            while (GameStart == true)
             {
-                
+
+                ConsoleKeyInfo ReadKey1;
+                ReadKey1 = Console.ReadKey(true);
+
+                Console.Clear();
+
+                string splitter = story[pg];
+                string[] page = splitter.Split(';');
+
+                if (ReadKey1.Key == ConsoleKey.A)
+                {
+                    if (page.Length > 2)
+                    {
+                        pg = Int32.Parse(page[3]);
+                    }
+ 
+                }
+                else if (ReadKey1.Key == ConsoleKey.D)
+                {
+                    if (page.Length > 2)
+                    {
+                        pg = Int32.Parse(page[4]);
+                    }
+
+                }
+                for (int i = 0; i < page.Length; i++)
+                {
+                    Console.WriteLine(page[i]);
+                }
+                //Console.ReadKey(true);
             }
             
 
 
-            
-           
+
+
+
+
+
         }
         static void ChoiceMaker()
         {
@@ -43,17 +84,7 @@ namespace CreateAStory
 
         static void Story()
         {
-            string[] story = { "FirstPage;FlipToPage2;FlipToPage3", "SecondPage;FlipToPage4;FlipToPage6", "ThirdPage;FlipToPage5;FlipToPage6", "4thPage", "5thPage", "6thPage" };
-            for (int i = 0; i < story.Length; i++)
-            {
-                string[] storysplitter = story[i].Split(';');
-
-                IDictionary<string, int> PageFlip = new Dictionary<string, int>();
-                PageFlip.Add(storysplitter[1], 3);
-                PageFlip.Add(storysplitter[2], 4);
-                PageFlip.Add(storysplitter[4], 9);
-                PageFlip.Add(storysplitter[5], 10);
-            }
+            
         }
     }
 }
